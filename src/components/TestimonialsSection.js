6e3d76cdae28d795808c48a30048dfaaf002c4b7
jasonbasuil <basuilj@gmail.com>
2020-06-15 12:33:31 -0700

    adds new student testimonials

--- src/components/TestimonialsSection.js
+++ src/components/TestimonialsSection.js
@@ -3,6 +3,9 @@ import Section from "./Section";
 import Container from "react-bootstrap/Container";
 import SectionHeader from "./SectionHeader";
 import Testimonials from "./Testimonials";
+import JuanPalominos from "../assets/JuanPalominos.png";
+import GiselleMondragon from "../assets/GiselleMondragon.png";
+import LibbePhan from "../assets/LibbePhan.jpg";
 
 function TestimonialsSection(props) {
   return (
@@ -43,6 +46,26 @@ function TestimonialsSection(props) {
               testimonial:
                 "The program helped me understand the importance of networking. I realize that it opens the door to so many opportunities like internships or jobs. It also exposed me to several different careers within the tech industry which I can see myself pursuing.  ",
               company: "D4X 2019 (Junior). Incoming @ Stanford University.",
+            },
+            {
+              avatar: GiselleMondragon,
+              name: "Giselle Mondragon",
+              testimonial: "What really stuck with me is the importance of first impressions and professionalism. I realized the importance of first impressions and communication after the program because I ended up getting a job interview for my school's bookstore. I was happy to know that I was very prepared for the interview and was not as nervous to ask questions or to answer them. I was able to get another interview for another opportunity right after!",
+              company: "D4X 2017 (Junior). Currently @ CSU Long Beach"
+            },
+            {
+              avatar: JuanPalominos,
+              name: "Juan Palominos",
+              testimonial:
+                `I've realized that I've changed my mindset. I used to be an intelligent student who was suffering from a lack of confidence. "What's the best thing that can happen?" and "What's the worst thing that can happen?" are some of my favorite phrases now that I ask myself whenever I find myself struggling to decide between two choices. I'm just excited now, excited to find out what else I can do now that I have a more open mindset!`,
+              company: "D4X 2017 (Junior). Currently @ Cal Poly SLO.",
+            },
+            {
+              avatar: LibbePhan,
+              name: "Libbe Phan",
+              testimonial:
+                `I'll forever be indebted to D4X. I don't how I can ever repay you all. When I told my friends who were interested in applying about this program, I don't think they really truly understood how much I gained from D4X. Hopefully my friends who ended up in cohort value all the work you guys do.`,
+              company: "D4X 2019 (Junior). Currently @ Irvine Valley College.",
             },
           ]}
         ></Testimonials>
