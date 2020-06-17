dab4336c1cb912c122f24cd65ee4546a60d341c2
jasonbasuil <basuilj@gmail.com>
2020-06-16 19:31:27 -0700

    update wording to Volunteer page

--- src/pages/volunteer.js
+++ src/pages/volunteer.js
@@ -10,9 +10,9 @@ function VolunteerPage(props) {
       size="md"
       bgImage=""
       bgImageOpacity={1}
-      title="Volunteer with us!"
+      title="Apply to be a mentor!"
       subtitle="We're always looking for committed individuals who believe in empowering young people. A great way to get involved with Destined for X is to volunteer to be a mentor. We are looking for early- to mid-career professionals to serve as mentors to our students to advise them in their high school/college careers. Being a mentor is a high-impact, low-time commitment opportunity and a great way to support the mission of Destined for X. If you are interested in becoming a mentor, please fill out our interest form below and we will reach out to you with more details!"
-      buttonText="Get Started"
+      buttonText="Apply now"
       buttonColor="primary"
       buttonOnClick={() => {
         // Navigate Mentor interest form
