36b9d441b11b0582488b4c6e886adea90854f3d2
jasonbasuil <basuilj@gmail.com>
2020-07-21 18:11:20 -0700

    new image to mentorship program page

--- src/pages/mentorship-program.js
+++ src/pages/mentorship-program.js
@@ -2,7 +2,7 @@ import React from "react";
 import HeroSection2 from "../components/HeroSection2";
 import FeaturesSection3 from "../components/FeaturesSection3";
 import CtaSection2 from "../components/CtaSection2";
-import undraw_people from "../assets/icons/undraw_people_tax5.svg"
+import FacebookTour from "../assets/events/FacebookTour.JPG";
 import TestimonialsSection2 from "../components/TestimonialsSection2";
 import FaqSection2 from "../components/FaqSection2";
 
@@ -25,7 +25,7 @@ function MentorshipProgramPage(props) {
         size="md"
         bgImage=""
         bgImageOpacity={1}
-        image={undraw_people}
+        image={FacebookTour}
       ></FeaturesSection3>
       <TestimonialsSection2
         bg="white"
