990ba394d701d7bafd88980896c53a387723c4d6
jasonbasuil <basuilj@gmail.com>
2020-08-07 22:11:20 -0700

    adds Moment.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
+
+import Container from "react-bootstrap/Container";
 import HeroSection2 from "./../components/HeroSection2";
+import Section from "./../components/Section";
+import SectionHeader from "./../components/SectionHeader";
 
 import Prismic from "prismic-javascript";
-import { RichText } from "prismic-reactjs";
+import { Date, RichText } from "prismic-reactjs";
+import Moment from "moment";
 
 const apiEndpoint = "https://destinedforx.cdn.prismic.io/api/v2";
 
@@ -11,6 +16,8 @@ const Client = Prismic.client(apiEndpoint);
 function BlogPage(props) {
   const [docs, setDocsData] = useState(null);
 
+  const formatDate = (date) => Moment(date).format("LL");
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await Client.query(
@@ -18,6 +25,7 @@ function BlogPage(props) {
       );
       if (response) {
         setDocsData(response.results);
+        console.log(response.results);
       }
     };
     fetchData();
@@ -26,7 +34,7 @@ function BlogPage(props) {
   return (
     <>
       <HeroSection2
-        bg="primary"
+        bg="secondary"
         textColor="white"
         size="lg"
         bgImage=""
@@ -34,13 +42,21 @@ function BlogPage(props) {
         title="Blog"
         subtitle="Welcome to our Blog"
       ></HeroSection2>
-
       {docs &&
         docs.map((doc) => (
-          <div>
-            <h1>{RichText.asText(doc.data.title)}</h1>
-            <RichText render={doc.data.body} />
-          </div>
+          <Section key={doc.id}>
+            <Container className="text-center">
+              <SectionHeader
+                title={RichText.asText(doc.data.title)}
+                subtitle={formatDate(Date(doc.data.timestamp).toString())}
+                size={2}
+              />
+            </Container>
+            <br></br>
+            <Container className="text-left">
+              <RichText render={doc.data.body} />
+            </Container>
+          </Section>
         ))}
     </>
   );
