abdbd5a4a8b4d116c0f986571db733abf88f0bdd
jasonbasuil <basuilj@gmail.com>
2020-09-30 21:36:08 -0700

    reverting props for MessengerCustomerChat

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -36,8 +36,6 @@ function App(props) {
             pageId="669786999881921"
             appId="768352100668005"
             themeColor="#366ba7"
-            loggedInGreeting="Hi! Have a question? We're here to help. "
-            greetingDialogDisplay="hide"
           />
           <Switch>
             <Route exact path="/" component={IndexPage} />
