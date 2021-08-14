82cac5a018d5b1d9eb2c5393ac94186e741f7104
jasonbasuil <basuilj@gmail.com>
2021-08-13 20:42:18 -0700

    redo deploy

--- src/pages/scholarship-program.js
+++ src/pages/scholarship-program.js
@@ -15,7 +15,7 @@ function ScholarshipProgramPage(props) {
         title="2021 Scholarship Program"
         subtitle="We are excited to announce that Destined for X will be awarding 3 scholarships this upcoming academic year. Learn more below."
       ></HeroSection2>
-      <div className="ScholarshipBannerContainer" style={{ textAlign: "center"}}>
+      <div className="ScholarshipBannerContainer " style={{ textAlign: "center"}}>
         <a href="https://forms.gle/s72icorBvWq6PDzi9">
           <img src={ScholarshipBanner} alt="Destined for X 2021 Scholarship Program" style={{ maxWidth: "100%", height: "auto"}} />
         </a>
