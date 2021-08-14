7033237560a2e33187d9de3103b37f904291a8fe
jasonbasuil <basuilj@gmail.com>
2021-08-13 20:34:46 -0700

    remove CTA section

--- src/pages/scholarship-program.js
+++ src/pages/scholarship-program.js
@@ -13,25 +13,13 @@ function ScholarshipProgramPage(props) {
         bgImage=""
         bgImageOpacity={1}
         title="2021 Scholarship Program"
-        subtitle="We are excited to announce that Destined for X will be awarding 3 scholarships this upcoming academic year."
+        subtitle="We are excited to announce that Destined for X will be awarding 3 scholarships this upcoming academic year. Learn more below."
       ></HeroSection2>
       <div className="ScholarshipBannerContainer" style={{ textAlign: "center"}}>
         <a href="https://forms.gle/s72icorBvWq6PDzi9">
           <img src={ScholarshipBanner} alt="Destined for X 2021 Scholarship Program" style={{ maxWidth: "100%", height: "auto"}} />
         </a>
       </div>
-      <CtaSection2
-        bg="dark"
-        textColor="white"
-        size="sm"
-        bgImage=""
-        bgImageOpacity={1}
-        buttonText="Apply Today"
-        buttonOnClick={() => {
-          window.open("https://forms.gle/s72icorBvWq6PDzi9");
-        }}
-        title="Applications are open"
-      />
     </>
   );
 }
